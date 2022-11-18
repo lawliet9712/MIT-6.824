30603eaaea7554a0b563784da260a392e2a83e22
lawliet9712 <www.wwj19@qq.com>
2022-11-18 22:19:45 +0800

    lab4a modify lang error

--- src/shardctrler/server.go
+++ src/shardctrler/server.go
@@ -1,11 +1,23 @@
 package shardctrler
 
+import (
+	"log"
+	"sync"
+	"time"
 
-import "6.824/raft"
-import "6.824/labrpc"
-import "sync"
-import "6.824/labgob"
+	"6.824/labgob"
+	"6.824/labrpc"
+	"6.824/raft"
+)
 
+const Debug = true
+
+func DPrintf(format string, a ...interface{}) (n int, err error) {
+	if Debug {
+		log.Printf(format, a...)
+	}
+	return
+}
 
 type ShardCtrler struct {
 	mu      sync.Mutex
@@ -15,20 +27,19 @@ type ShardCtrler struct {
 
 	// Your data here.
 	requestMap map[int64]*ShardClerk // ckid to seqid for filter repeat request
-	configs []Config // indexed by config num
+	configs    []Config              // indexed by config num
 }
 
 type ShardClerk struct {
 	seqId       int
-	ckId  	    int64
 	msgUniqueId int
 	messageCh   chan Op
 }
 
 const (
-	T_Join = "Join"
+	T_Join  = "Join"
 	T_Leave = "Leave"
-	T_Move = "Move"
+	T_Move  = "Move"
 	T_Query = "Query"
 )
 
@@ -38,31 +49,31 @@ type T_Op string
 type Op struct {
 	// Your data here.
 	Command T_Op
-	SeqId int
+	SeqId   int
 	Servers map[int][]string // T_Join
-	GIDs []int // T_Remove
-	Shard int // T_Move
-	GID int // T_Move
+	GIDs    []int            // T_Remove
+	Shard   int              // T_Move
+	GID     int              // T_Move
+	CkId    int64
 }
 
 func (sc *ShardCtrler) GetCk(ckId int64) *ShardClerk {
 	ck, found := sc.requestMap[ckId]
 	if !found {
-		ck = new(ClerkOps)
+		ck = new(ShardClerk)
 		ck.messageCh = make(chan Op)
-		ck.mu = &sc.mu
 		sc.requestMap[ckId] = ck
 		DPrintf("[ShardCtrler-%d] Init ck %d", sc.me, ckId)
 	}
-	return sc.
+	return sc.requestMap[ckId]
 }
 
-func (sc *ShardCtrler) WaitApplyMsgByCh(ck *ShardClerk) (Op, Err) {
+func (sc *ShardCtrler) WaitApplyMsgByCh(ck *ShardClerk) (Op, bool) {
 	startTerm, _ := sc.rf.GetState()
 	timer := time.NewTimer(1000 * time.Millisecond)
 	for {
 		select {
-		case Msg := <-ch:
+		case Msg := <-ck.messageCh:
 			return Msg, true
 		case <-timer.C:
 			curTerm, isLeader := sc.rf.GetState()
@@ -78,7 +89,7 @@ func (sc *ShardCtrler) WaitApplyMsgByCh(ck *ShardClerk) (Op, Err) {
 }
 
 func (sc *ShardCtrler) NotifyApplyMsgByCh(ch chan Op, Msg Op) {
-	// we wait 200ms 
+	// we wait 200ms
 	// if notify timeout, then we ignore, because client probably send request to anthor server
 	timer := time.NewTimer(200 * time.Millisecond)
 	select {
@@ -93,80 +104,12 @@ func (sc *ShardCtrler) NotifyApplyMsgByCh(ch chan Op, Msg Op) {
 
 func (sc *ShardCtrler) Join(args *JoinArgs, reply *JoinReply) {
 	// Your code here.
-	kv.mu.Lock()
-	logIndex, _, isLeader := sc.rf.Start(Op{
-		Servers : args.Servers,
-		SeqId : args.seqId,
-		Command : T_Join,
-	})
-	if !isLeader {
-		reply.WrongLeader = true
-		kv.mu.Unlock()
-		return
-	}
-
-	ck := sc.GetCk(args.CkId)
-	ck.msgUniqueId = logIndex
-	kv.mu.Unlock()
-
-	Msg, WrongLeader := sc.WaitApplyMsgByCh(ck)
-	reply.WrongLeader = WrongLeader
-	return
-}
-
-func (sc *ShardCtrler) Leave(args *LeaveArgs, reply *LeaveReply) {
-	// Your code here.
-	kv.mu.Lock()
-	logIndex, _, isLeader := sc.rf.Start(Op{
-		GIDs : args.GIDs,
-		SeqId : args.seqId,
-		Command : T_Leave,
-	})
-	if !isLeader {
-		reply.WrongLeader = true
-		kv.mu.Unlock()
-		return
-	}
-
-	ck := sc.GetCk(args.CkId)
-	ck.msgUniqueId = logIndex
-	kv.mu.Unlock()
-
-	Msg, WrongLeader := sc.WaitApplyMsgByCh(ck)
-	reply.WrongLeader = WrongLeader
-	return
-}
-
-func (sc *ShardCtrler) Move(args *MoveArgs, reply *MoveReply) {
-	// Your code here.
-	kv.mu.Lock()
-	logIndex, _, isLeader := sc.rf.Start(Op{
-		Shard : args.Shard,
-		GID : args.GID,
-		SeqId : args.seqId,
-		Command : T_Move,
-	})
-	if !isLeader {
-		reply.WrongLeader = true
-		kv.mu.Unlock()
-		return
-	}
-
-	ck := sc.GetCk(args.CkId)
-	ck.msgUniqueId = logIndex
-	kv.mu.Unlock()
-
-	Msg, WrongLeader := sc.WaitApplyMsgByCh(ck)
-	reply.WrongLeader = WrongLeader
-	return
-}
-
-func (sc *ShardCtrler) Query(args *QueryArgs, reply *QueryReply) {
-	// Your code here.
 	sc.mu.Lock()
 	logIndex, _, isLeader := sc.rf.Start(Op{
-		SeqId : args.seqId,
-		Command : T_Query,
+		Servers: args.Servers,
+		SeqId:   args.SeqId,
+		Command: T_Join,
+		CkId:    args.CkId,
 	})
 	if !isLeader {
 		reply.WrongLeader = true
@@ -178,19 +121,87 @@ func (sc *ShardCtrler) Query(args *QueryArgs, reply *QueryReply) {
 	ck.msgUniqueId = logIndex
 	sc.mu.Unlock()
 
-	Msg, WrongLeader := sc.WaitApplyMsgByCh(ck)
+	_, WrongLeader := sc.WaitApplyMsgByCh(ck)
+	reply.WrongLeader = WrongLeader
+}
+
+func (sc *ShardCtrler) Leave(args *LeaveArgs, reply *LeaveReply) {
+	// Your code here.
+	sc.mu.Lock()
+	logIndex, _, isLeader := sc.rf.Start(Op{
+		GIDs:    args.GIDs,
+		SeqId:   args.SeqId,
+		Command: T_Leave,
+		CkId:    args.CkId,
+	})
+	if !isLeader {
+		reply.WrongLeader = true
+		sc.mu.Unlock()
+		return
+	}
+
+	ck := sc.GetCk(args.CkId)
+	ck.msgUniqueId = logIndex
+	sc.mu.Unlock()
+
+	_, WrongLeader := sc.WaitApplyMsgByCh(ck)
+	reply.WrongLeader = WrongLeader
+}
+
+func (sc *ShardCtrler) Move(args *MoveArgs, reply *MoveReply) {
+	// Your code here.
+	sc.mu.Lock()
+	logIndex, _, isLeader := sc.rf.Start(Op{
+		Shard:   args.Shard,
+		GID:     args.GID,
+		SeqId:   args.SeqId,
+		Command: T_Move,
+		CkId:    args.CkId,
+	})
+	if !isLeader {
+		reply.WrongLeader = true
+		sc.mu.Unlock()
+		return
+	}
+
+	ck := sc.GetCk(args.CkId)
+	ck.msgUniqueId = logIndex
+	sc.mu.Unlock()
+
+	_, WrongLeader := sc.WaitApplyMsgByCh(ck)
+	reply.WrongLeader = WrongLeader
+}
+
+func (sc *ShardCtrler) Query(args *QueryArgs, reply *QueryReply) {
+	// Your code here.
+	sc.mu.Lock()
+	logIndex, _, isLeader := sc.rf.Start(Op{
+		SeqId:   args.SeqId,
+		Command: T_Query,
+		CkId:    args.CkId,
+	})
+	if !isLeader {
+		reply.WrongLeader = true
+		sc.mu.Unlock()
+		return
+	}
+
+	ck := sc.GetCk(args.CkId)
+	ck.msgUniqueId = logIndex
+	sc.mu.Unlock()
+
+	_, WrongLeader := sc.WaitApplyMsgByCh(ck)
 	sc.mu.Lock()
 	defer sc.mu.Unlock()
 	reply.WrongLeader = WrongLeader
 	reply.Config = sc.getConfig(args.Num)
-	DPrintf("[ShardCtrler-%d] %d Clerk-%d Do [Query] Reply Config=%v", sc.me, args.CkId, reply.Config)
-	return
+	DPrintf("[ShardCtrler-%d] Clerk-%d Do [Query] Reply Config=%v", sc.me, args.CkId, reply.Config)
 }
 
 func (sc *ShardCtrler) getGIDs() []int {
 	conf := sc.getConfig(-1)
-	gids := make([]int)
-	for gid, _ := range(conf.Groups) {
+	gids := make([]int, 0)
+	for gid, _ := range conf.Groups {
 		gids = append(gids, gid)
 	}
 	return gids
@@ -198,7 +209,7 @@ func (sc *ShardCtrler) getGIDs() []int {
 
 func (sc *ShardCtrler) getConfig(confNumber int) Config {
 	if confNumber == -1 || confNumber >= len(sc.configs) {
-		return sc.configs[len(sc.configs) - 1]
+		return sc.configs[len(sc.configs)-1]
 	}
 	return sc.configs[confNumber]
 }
@@ -211,30 +222,30 @@ func (sc *ShardCtrler) invokeMsg(Msg Op) {
 	switch Msg.Command {
 	case T_Join: // add a set of groups
 		DPrintf("[ShardCtrler-%d] Do T_Join, Msg=%v", sc.me, Msg)
-		latestConf = sc.getConfig(-1))
+		latestConf := sc.getConfig(-1)
 		newGroups := make(map[int][]string)
 		// merge new group
-		for gid, server := range(Msg.Servers) {
+		for gid, servers := range Msg.Servers {
 			newGroups[gid] = servers
 		}
 		// merge old group
-		for gid, server := range(latestConf.Groups) {
-			newGroups[gid] := servers
+		for gid, servers := range latestConf.Groups {
+			newGroups[gid] = servers
 		}
 		// append new config
 		config := Config{
-			Num : len(sc.configs),
-			Groups : Msg.Servers,
-			Shards : latestConf.Shards,
+			Num:    len(sc.configs),
+			Groups: Msg.Servers,
+			Shards: latestConf.Shards,
 		}
 		sc.configs = append(sc.configs, config)
 		// maybe need rebalance now
 		sc.rebalance()
-	case T_Leave:  // delete a set of groups
+	case T_Leave: // delete a set of groups
 		DPrintf("[ShardCtrler-%d] Do T_Leave, Msg=%v", sc.me, Msg)
 		latestConf := sc.getConfig(-1)
 		newGroups := make(map[int][]string)
-		for gid, servers range (latestConf.Groups) {
+		for gid, servers := range latestConf.Groups {
 			// not in the remove gids, then append to new config
 			if !xIsInGroup(gid, Msg.GIDs) {
 				newGroups[gid] = servers
@@ -242,9 +253,9 @@ func (sc *ShardCtrler) invokeMsg(Msg Op) {
 		}
 		// append new config
 		config := Config{
-			Num : len(sc.configs),
-			Groups : Msg.Servers,
-			Shards : latestConf.Shards,
+			Num:    len(sc.configs),
+			Groups: Msg.Servers,
+			Shards: latestConf.Shards,
 		}
 		sc.configs = append(sc.configs, config)
 		sc.rebalance()
@@ -252,9 +263,9 @@ func (sc *ShardCtrler) invokeMsg(Msg Op) {
 		DPrintf("[ShardCtrler-%d] Do T_Move, Msg=%v", sc.me, Msg)
 		latestConf := sc.getConfig(-1)
 		config := Config{
-			Num : len(sc.configs),
-			Groups : latestConf.Groups,
-			Shards : latestConf.Shards,
+			Num:    len(sc.configs),
+			Groups: latestConf.Groups,
+			Shards: latestConf.Shards,
 		}
 		config.Shards[Msg.Shard] = Msg.GID // no need rebalance
 	case T_Query:
@@ -273,12 +284,12 @@ func (sc *ShardCtrler) processMsg() {
 		ck := sc.GetCk(opMsg.CkId)
 		// already process
 		DPrintf("[ShardCtrler-%d] Received Msg %v", sc.me, applyMsg)
-		if opMsg.seqId < ck.seqId {
+		if opMsg.SeqId < ck.seqId {
 			sc.mu.Unlock()
 			continue
 		}
-		
-		_, isLeader = sc.rf.GetState()
+
+		_, isLeader := sc.rf.GetState()
 		if applyMsg.CommandIndex == ck.msgUniqueId && isLeader {
 			sc.NotifyApplyMsgByCh(ck.messageCh, opMsg)
 			ck.msgUniqueId = 0
@@ -288,12 +299,10 @@ func (sc *ShardCtrler) processMsg() {
 	}
 }
 
-//
 // the tester calls Kill() when a ShardCtrler instance won't
 // be needed again. you are not required to do anything
 // in Kill(), but it might be convenient to (for example)
 // turn off debug output from this instance.
-//
 func (sc *ShardCtrler) Kill() {
 	sc.rf.Kill()
 	// Your code here, if desired.
@@ -304,12 +313,10 @@ func (sc *ShardCtrler) Raft() *raft.Raft {
 	return sc.rf
 }
 
-//
 // servers[] contains the ports of the set of
 // servers that will cooperate via Raft to
 // form the fault-tolerant shardctrler service.
 // me is the index of the current server in servers[].
-//
 func StartServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister) *ShardCtrler {
 	sc := new(ShardCtrler)
 	sc.me = me
@@ -322,7 +329,7 @@ func StartServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister)
 	sc.rf = raft.Make(servers, me, persister, sc.applyCh)
 
 	// Your code here.
-	sc.requestMap = make(map[int64]int)
+	sc.requestMap = make(map[int64]*ShardClerk)
 	go sc.processMsg()
 	return sc
 }
